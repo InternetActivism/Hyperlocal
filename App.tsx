42f9db83f3f86532d3519aa2373cf993a7b322c9
Adrian Gri <adrian.gri03@gmail.com>
2022-11-10 17:24:20 -0500

    switch to typescript

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type {Node} from 'react';
+import type {ReactNode} from 'react';
 import {
   NativeModules,
   SafeAreaView,
@@ -7,21 +7,26 @@ import {
   Text,
   View,
   NativeEventEmitter,
+  EmitterSubscription,
 } from 'react-native';
 
 import {Button, Input} from '@rneui/themed';
 
-let startListener,
-  failedStartListener,
-  didConnectListener,
-  didDisconnectListener,
-  messageSentListener,
-  messageSentFailedListener,
-  messageReceivedListener;
+let startListener: EmitterSubscription,
+  failedStartListener: EmitterSubscription,
+  didConnectListener: EmitterSubscription,
+  didDisconnectListener: EmitterSubscription,
+  messageSentListener: EmitterSubscription,
+  messageSentFailedListener: EmitterSubscription,
+  messageReceivedListener: EmitterSubscription;
 
-let connectedUsers = [];
+let connectedUsers: string[] = [];
 
-const createListeners = (eventEmitter, onConnect, onRecieve) => {
+const createListeners = (
+  eventEmitter: NativeEventEmitter,
+  onConnect: (userID: string) => void,
+  onRecieve: (text: string) => void,
+) => {
   startListener = eventEmitter.addListener('onDidStart', data => {
     console.log('sdk did start with data: ', data);
   });
@@ -66,21 +71,21 @@ const createListeners = (eventEmitter, onConnect, onRecieve) => {
 
 // need to call this when component unmounts
 // might not actually need this since we always need these
-const destroyListeners = () => {
-  startListener.remove();
-  failedStartListener.remove();
-  didConnectListener.remove();
-  didDisconnectListener.remove();
-  messageSentListener.remove();
-  messageSentFailedListener.remove();
-  messageReceivedListener.remove();
-};
+// const destroyListeners = () => {
+//   startListener.remove();
+//   failedStartListener.remove();
+//   didConnectListener.remove();
+//   didDisconnectListener.remove();
+//   messageSentListener.remove();
+//   messageSentFailedListener.remove();
+//   messageReceivedListener.remove();
+// };
 
-const App: () => Node = () => {
+const App: () => ReactNode = () => {
   const BridgefySwift = NativeModules.BridgefySwift;
-  const [message, setMessage] = React.useState('');
-  const [connections, setConnections] = React.useState([]);
-  const [messagesRecieved, setMessagesRecieved] = React.useState([]);
+  const [message, setMessage] = React.useState<string>('');
+  const [connections, setConnections] = React.useState<string[]>([]);
+  const [messagesRecieved, setMessagesRecieved] = React.useState<string[]>([]);
 
   const onConnect = (userID: string) => {
     if (!connections.includes(userID)) {
@@ -96,7 +101,7 @@ const App: () => Node = () => {
   createListeners(BridgefySwiftEmit, onConnect, onRecieve);
 
   const onPressSDK = () => {
-    BridgefySwift.startSDK(result => {
+    BridgefySwift.startSDK((result: any) => {
       console.log(result);
     });
   };
@@ -110,21 +115,31 @@ const App: () => Node = () => {
       console.log('No message');
       return;
     }
-    BridgefySwift.sendMessage(message, connectedUsers[0], result => {
+    BridgefySwift.sendMessage(message, connectedUsers[0], (result: any) => {
       console.log(result);
     });
   };
 
   const ConnectedUsersText = () => {
-    return connectedUsers.map(user => {
-      return <Text key={user}>{user}</Text>;
-    });
+    return (
+      <View>
+        {connectedUsers.map(user => {
+          return <Text key={user}>{user}</Text>;
+        })}
+      </View>
+    );
   };
 
   const MessagesRecievedText = () => {
-    return messagesRecieved.map(text => {
-      return <Text key={text + messagesRecieved.indexOf(text)}>{text}</Text>;
-    });
+    return (
+      <View>
+        {messagesRecieved.map(text => {
+          return (
+            <Text key={text + messagesRecieved.indexOf(text)}>{text}</Text>
+          );
+        })}
+      </View>
+    );
   };
 
   return (
