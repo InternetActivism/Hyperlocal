fd524b6852c11984c8888cccddececc775bacb69
Krish Shah <krish.life21@gmail.com>
2022-11-25 06:49:02 -0500

    added icons, need to be updated in the future

--- src/pages/TabNavigator/TabNavigator.tsx
+++ src/pages/TabNavigator/TabNavigator.tsx
@@ -3,21 +3,43 @@ import React from 'react';
 import { ConversationsNavigation, SamplePage, DiscoverPage } from '../../pages';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet } from 'react-native';
+import PeopleIcon from '../../components/ui/Icons/PeopleIcon/PeopleIcon';
+import PeopleIconSelected from '../../components/ui/Icons/PeopleIcon/PeopleIconSelected';
+import MessageIconSelected from '../../components/ui/Icons/MessageIcon/MessageIconSelected';
+import MessageIcon from '../../components/ui/Icons/MessageIcon/MessageIcon';
+import ChevronRightIcon from '../../components/ui/Icons/ChevronRightIcon';
 
 export default function TabNavigator() {
   const Tab = createBottomTabNavigator();
 
   return (
     <Tab.Navigator
-      initialRouteName="ConversationsNavigation"
-      screenOptions={{ headerShown: false }}
+      initialRouteName="Discover"
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarLabel: () => {
+          return null;
+        },
+        tabBarIcon: ({ focused }) => {
+          let iconComponent = <ChevronRightIcon />;
+
+          if (route.name === 'Discover') {
+            iconComponent = focused ? <PeopleIconSelected /> : <PeopleIcon />;
+          } else if (route.name === 'ConversationsNavigation') {
+            iconComponent = focused ? <MessageIconSelected /> : <MessageIcon />;
+          }
+
+          // You can return any component that you like here!
+          return iconComponent;
+        },
+      })}
       sceneContainerStyle={styles.container}>
       <Tab.Screen name="Sample" component={SamplePage} />
+      <Tab.Screen name="Discover" component={DiscoverPage} />
       <Tab.Screen
         name="ConversationsNavigation"
         component={ConversationsNavigation}
       />
-      <Tab.Screen name="Discover" component={DiscoverPage} />
     </Tab.Navigator>
   );
 }
