b0915825c7de0de89fe686855a64e12150d985ea
Krish Shah <krish.life21@gmail.com>
2022-12-20 20:14:44 -0700

    did pod install, fixed bugs, loading page emulator

--- src/pages/Loading/LoadingPage.tsx
+++ src/pages/Loading/LoadingPage.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import { useAtom } from 'jotai';
+import React, { useEffect } from 'react';
 import { ActivityIndicator, SafeAreaView, StyleSheet, View } from 'react-native';
-
+import { currentUserInfoAtom } from '../../services/atoms';
+import { generateRandomName } from '../../utils/RandomName/generateRandomName';
 // import BluetoothStateManager from 'react-native-bluetooth-state-manager';
 
 const LoadingPage = () => {
+  const [userInfo, setUserInfo] = useAtom(currentUserInfoAtom);
+
   // const isBluetoothEnabled = async () => {
   //   const state = await BluetoothStateManager.getState();
   //   console.log('Bluetooth state: ', state);
@@ -14,12 +18,37 @@ const LoadingPage = () => {
   //   }
   // };
 
+  /* Sets up a timeout to start the SDK. Allows us to run the app in a simulator. */
+  useEffect(() => {
+    var timer: any = null;
+    timer = setTimeout(() => {
+      if (__DEV__) {
+        console.log('running in dev mode');
+      }
+      if (__DEV__ && !userInfo) {
+        setUserInfo({
+          userID: '698E84AE-67EE-4057-87FF-788F88069B68',
+          nickname: generateRandomName(),
+          userFlags: 0,
+          privacy: 0, // used in future versions
+          verified: false, // used in future versions
+          dateCreated: Date.now(),
+          dateUpdated: Date.now(),
+        });
+
+        return;
+      }
+      throw Error('Error on loading user info.');
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, [userInfo, setUserInfo]);
+
   /*
 
     Checks are first start (check via user_info), Bluetooth, SDK ready.
 
   */
-
   return (
     <SafeAreaView>
       <View style={styles.container}>
