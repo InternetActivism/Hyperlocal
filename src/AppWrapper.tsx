09c7b60254535275b18ea4b71dce09b993ffd6a5
Adrian Gri <adrian.gri03@gmail.com>
2022-12-30 16:00:03 -0500

    added new splash screens

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'jotai';
 import App from './App';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function AppWrapper() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider>
       <App />
