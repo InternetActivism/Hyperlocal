c10b8566bb73505d4ddee708301f7eee1b68817b
Adrian Gri <33498069+AdrianGri@users.noreply.github.com>
2023-03-21 19:11:14 -0700

    HYP-102: Rework public chat storage (#52)

--- src/pages/PublicChatPage/PublicChatPage.tsx
+++ src/pages/PublicChatPage/PublicChatPage.tsx
@@ -1,4 +1,4 @@
-import { useAtom } from 'jotai';
+import { useAtom, useAtomValue, useSetAtom } from 'jotai';
 import { default as React, useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import { PublicChatHeader } from '../../components/features/PublicChat';
@@ -9,12 +9,12 @@ import {
   getActiveConnectionsAtom,
   publicChatCacheAtom,
 } from '../../services/atoms';
-import { StoredPublicChatMessage } from '../../services/database';
-import { setMessageWithID } from '../../services/message_storage';
 import {
-  expirePublicPendingMessages,
-  getPublicChatConversation,
-} from '../../services/public_messages';
+  addMessageToPublicChatAtom,
+  expirePublicPendingMessagesAtom,
+  updateMessageInPublicChatAtom,
+} from '../../services/atoms/public_chat';
+import { StoredPublicChatMessage } from '../../services/database';
 import { sendPublicChatMessageWrapper } from '../../services/transmission';
 import { MessageStatus, MESSAGE_PENDING_EXPIRATION_TIME } from '../../utils/globals';
 import { vars } from '../../utils/theme';
@@ -25,10 +25,12 @@ interface Props {
 
 const PublicChatPage = ({ navigation }: Props) => {
   const [userInfo] = useAtom(currentUserInfoAtom);
-  const [publicChatCache, setPublicChatCache] = useAtom(publicChatCacheAtom);
+  const publicChatCache = useAtomValue(publicChatCacheAtom);
   const [connections] = useAtom(getActiveConnectionsAtom);
   const [numConnected, setNumConnected] = useState<number>(0);
-  const [messages, setMessages] = useState<StoredPublicChatMessage[]>([]);
+  const addMessageToPublicChat = useSetAtom(addMessageToPublicChatAtom);
+  const updateMessageInPublicChat = useSetAtom(updateMessageInPublicChatAtom);
+  const expirePendingPublicMessages = useSetAtom(expirePublicPendingMessagesAtom);
 
   // TODO: Fix this later cause page refresh when allContacts changes.
   // Runs on mount. Sets up the chat page.
@@ -38,7 +40,7 @@ const PublicChatPage = ({ navigation }: Props) => {
     }
 
     // Check for pending messages that need to be expired.
-    updateExpiredMessages();
+    expirePendingPublicMessages();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -47,13 +49,6 @@ const PublicChatPage = ({ navigation }: Props) => {
   useEffect(() => {
     setNumConnected(connections.length);
   }, [connections]);
-
-  // Update local messages when conversation cache changes.
-  useEffect(() => {
-    if (publicChatCache) {
-      setMessages(publicChatCache.history);
-    }
-  }, [publicChatCache]);
 
   // Runs when a user clicks on a failed message to retry sending it.
   const sendMessageAgain = async (message: StoredPublicChatMessage) => {
@@ -72,17 +67,20 @@ const PublicChatPage = ({ navigation }: Props) => {
     // Hide the old message to be retried.
     // Doesn't actually delete the message from the database, just hides it.
     message.statusFlag = MessageStatus.DELETED;
-    setMessageWithID(message.messageID, message);
+    updateMessageInPublicChat({ messageID: message.messageID, message });
 
     // Retry sending message with the same content.
-    await sendPublicChatMessageWrapper(userInfo.nickname, userInfo.userID, message.content);
+    const sentMessage: StoredPublicChatMessage = await sendPublicChatMessageWrapper(
+      userInfo.nickname,
+      userInfo.userID,
+      message.content
+    );
 
-    // Update conversation cache with the new pending message and the old message hidden, using the database as a reference.
-    setPublicChatCache({ history: getPublicChatConversation(), lastUpdated: Date.now() });
+    addMessageToPublicChat(sentMessage);
 
     // Check back in a few seconds to see if the message has failed to go through.
     // This is needed since Bridgefy doesn't always let us know if a message failed to send via messageFailedToSend.
-    setTimeout(() => updateExpiredMessages(), MESSAGE_PENDING_EXPIRATION_TIME);
+    setTimeout(() => expirePendingPublicMessages(), MESSAGE_PENDING_EXPIRATION_TIME);
   };
 
   // Send message to contact. Assumes contact exists.
@@ -92,32 +90,22 @@ const PublicChatPage = ({ navigation }: Props) => {
     }
 
     // Send message via Bridgefy.
-    await sendPublicChatMessageWrapper(userInfo?.nickname, userInfo.userID, text);
+    const sentMessage: StoredPublicChatMessage = await sendPublicChatMessageWrapper(
+      userInfo?.nickname,
+      userInfo.userID,
+      text
+    );
 
-    // Update conversation cache with new pending message.
-    // This'll be updated to a sent message once the message is confirmed to have been sent via the onMessageSent callback.
-    // Find that in the App.tsx file.
-    setPublicChatCache({ history: getPublicChatConversation(), lastUpdated: Date.now() });
+    addMessageToPublicChat(sentMessage);
 
     // Check back in a few seconds to see if the message has failed to go through.
     // This is needed since Bridgefy doesn't always let us know if a message failed to send via messageFailedToSend.
-    setTimeout(() => updateExpiredMessages(), MESSAGE_PENDING_EXPIRATION_TIME);
-  };
-
-  const updateExpiredMessages = () => {
-    // Check for any pending messages that have expired.
-    const didExpire = expirePublicPendingMessages();
-
-    // If any pending messages have expired, update the conversation cache.
-    // This will cause the chat page to re-render.
-    if (didExpire) {
-      setPublicChatCache({ history: getPublicChatConversation(), lastUpdated: Date.now() });
-    }
+    setTimeout(() => expirePendingPublicMessages(), MESSAGE_PENDING_EXPIRATION_TIME);
   };
 
   // Render the bubbles in the chat.
   const renderBubbles = (): JSX.Element => {
-    if (!messages || messages.length === 0) {
+    if (!publicChatCache.history || publicChatCache.history.length === 0) {
       return <View />;
     }
 
@@ -125,7 +113,7 @@ const PublicChatPage = ({ navigation }: Props) => {
     // This is updated when the conversation cache changes.
     return (
       <>
-        {messages.map((message: StoredPublicChatMessage) => {
+        {publicChatCache.history.map((message: StoredPublicChatMessage) => {
           // Do not show deleted messages and nickname change messages.
           if (message.statusFlag === MessageStatus.DELETED) {
             return null;
---
+++ src/services/atoms/public_chat.ts
@@ -0,0 +1,61 @@
+import { atom } from 'jotai';
+import { publicChatCacheAtom, publicChatInfoAtom } from '../atoms';
+import { StoredPublicChatMessage } from '../database';
+import { fetchConversation, setMessageWithID } from '../message_storage';
+import { expirePublicPendingMessages, savePublicChatMessageToStorage } from '../public_messages';
+
+export const addMessageToPublicChatAtom = atom(
+  null,
+  (get, set, update: StoredPublicChatMessage) => {
+    const publicChatInfo = get(publicChatInfoAtom);
+
+    if (publicChatInfo.lastMsgPointer) {
+      set(publicChatInfoAtom, {
+        ...publicChatInfo,
+        lastMsgPointer: update.messageID,
+      });
+    } else {
+      if (publicChatInfo.firstMsgPointer) {
+        throw new Error('firstMsgPointer is defined but lastMsgPointer is not');
+      }
+      set(publicChatInfoAtom, {
+        ...publicChatInfo,
+        lastMsgPointer: update.messageID,
+        firstMsgPointer: update.messageID,
+      });
+    }
+
+    savePublicChatMessageToStorage(publicChatInfo, update);
+    set(syncPublicChatInCacheAtom);
+  }
+);
+
+export const updateMessageInPublicChatAtom = atom(
+  null,
+  (get, set, update: { messageID: string; message: StoredPublicChatMessage }) => {
+    setMessageWithID(update.messageID, update.message);
+    set(syncPublicChatInCacheAtom);
+  }
+);
+
+export const expirePublicPendingMessagesAtom = atom(null, (get, set) => {
+  const chatInfo = get(publicChatInfoAtom);
+  const didExpire = expirePublicPendingMessages(chatInfo);
+
+  if (didExpire) {
+    set(syncPublicChatInCacheAtom);
+  }
+});
+
+export const syncPublicChatInCacheAtom = atom(null, (get, set) => {
+  const publicChatInfo = get(publicChatInfoAtom);
+  if (!publicChatInfo.lastMsgPointer || !publicChatInfo.firstMsgPointer) {
+    set(publicChatCacheAtom, { history: [], lastUpdated: Date.now() });
+    return;
+  }
+
+  const history: StoredPublicChatMessage[] = fetchConversation(
+    publicChatInfo.lastMsgPointer
+  ) as StoredPublicChatMessage[];
+  set(publicChatCacheAtom, { history, lastUpdated: Date.now() });
+});
