effa2c755725ebf797a788db7cb03c1b5e184bc1
Will DePue <will@depue.net>
2023-03-28 12:36:48 -0700

    Fix last seen counter

--- src/utils/timeSinceTimestamp.ts
+++ src/utils/timeSinceTimestamp.ts
@@ -19,8 +19,5 @@ export function timeSinceTimestamp(timestamp: number) {
     return Math.floor(interval) + ' hours ago';
   }
   interval = seconds / 60;
-  if (interval > 1) {
-    return Math.floor(interval) + ' mins ago';
-  }
-  return Math.floor(seconds) + ' secs ago';
+  return Math.floor(Math.min(interval, 1)) + ' mins ago';
 }
