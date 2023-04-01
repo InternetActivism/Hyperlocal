8a8ba8399703488ae7ef9abd3119bab6cd7adf02
Adrian Gri <33498069+AdrianGri@users.noreply.github.com>
2023-03-31 18:00:39 -0700

    Fix button ripple effect

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,30 +1,39 @@
 import { Button as RneuiButton } from '@rneui/base';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { vars } from '../../utils/theme';
+
+const BORDER_RADIUS = 32;
 
 const Button = (props: any) => {
   return (
-    <RneuiButton
-      buttonStyle={[styles.buttonStyleDefault, props.style]}
-      titleStyle={styles.textStyleDefault}
-      disabledTitleStyle={styles.textStyleDisabled}
-      disabledStyle={styles.buttonStyleDisabled}
-      {...props}
-    />
+    <View style={styles.buttonWrapper}>
+      <RneuiButton
+        buttonStyle={[styles.buttonStyleDefault, props.style]}
+        titleStyle={styles.textStyleDefault}
+        disabledTitleStyle={styles.textStyleDisabled}
+        disabledStyle={styles.buttonStyleDisabled}
+        {...props}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonWrapper: {
+    borderRadius: BORDER_RADIUS,
+    overflow: 'hidden',
+  },
   buttonStyleDefault: {
     backgroundColor: vars.primaryColor.button,
-    borderRadius: 32,
+    borderRadius: BORDER_RADIUS,
     height: 50,
     width: 330,
+    overflow: 'hidden',
   },
   buttonStyleDisabled: {
     backgroundColor: vars.black.soft,
-    borderRadius: 32,
+    borderRadius: BORDER_RADIUS,
     height: 50,
     width: 330,
   },
