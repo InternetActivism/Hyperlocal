4a4e7dd1ad5029893a78202c410002dc6aaee195
Adrian Gri <33498069+AdrianGri@users.noreply.github.com>
2023-04-08 13:55:34 -0400

    Fix double stop

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import {
   currentViewAtom,
 } from './services/atoms';
 import { startSDK, stopSDK } from './services/bridgefy-link';
-import { BridgefyErrorStates } from './utils/globals';
+import { BridgefyErrorStates, BridgefyStates } from './utils/globals';
 import { vars } from './utils/theme';
 
 export type RootStackParamList = {
@@ -132,7 +132,7 @@ export default function App(): JSX.Element {
       startSDK().catch((e) => console.error(e));
     }
 
-    if (appStateVisible.match(/inactive|background/)) {
+    if (appStateVisible.match(/inactive|background/) && bridgefyStatus !== BridgefyStates.OFFLINE) {
       stopSDK().catch((e) => console.error(e));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
