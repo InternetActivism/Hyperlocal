f026335e98b2c9440f8a8712084ac1667558309e
Krish Shah <50386081+KrishKrosh@users.noreply.github.com>
2023-04-10 22:48:14 -0700

    Revert "Revert "Always center text in profile pictures"" (#106)

--- src/components/ui/ProfilePicture.tsx
+++ src/components/ui/ProfilePicture.tsx
@@ -1,115 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import { Circle, Defs, LinearGradient, Stop, Svg, Text } from 'react-native-svg';
 import { vars } from '../../utils/theme';
+
+const sizes = {
+  xs: {
+    size: 38,
+    fontSize: 20,
+  },
+  sm: {
+    size: 42,
+    fontSize: 22,
+  },
+  md: {
+    size: 65,
+    fontSize: 32,
+  },
+  lg_s: {
+    size: 68,
+    fontSize: 36,
+  },
+  lg: {
+    size: 80,
+    fontSize: 42,
+  },
+  xl: {
+    size: 122,
+    fontSize: 60,
+  },
+};
 
 const ProfilePicture = ({
   size,
   title,
-  extraStyle,
 }: {
   size: 'xs' | 'sm' | 'md' | 'lg_s' | 'lg' | 'xl';
   title: string;
-  extraStyle?: any;
 }) => {
   const avatarTitle = title
     .split(' ')
     .map((word) => word.charAt(0))
     .join('')
-    .slice(0, 2);
+    .slice(0, 2)
+    .toUpperCase();
 
-  const container = containerStyles[size];
+  const length = sizes[size].size;
+  const fontSize = sizes[size].fontSize;
 
   return (
-    <View style={[container, extraStyle, containerStyles.main]}>
-      <Text style={[textStyles[size], textStyles.main]}>{avatarTitle}</Text>
-      <View style={[containerStyles.background, container]}>
-        <LinearGradient colors={['#00000000', '#000000']} style={containerStyles.gradient} />
-      </View>
-    </View>
+    <Svg width={length} height={length} viewBox={`0 0 ${length} ${length}`}>
+      <Defs>
+        <LinearGradient id="grad" x1="0" y1="0" x2="0" y2={length} gradientUnits="userSpaceOnUse">
+          <Stop offset="0" stopColor="transparent" stopOpacity="0" />
+          <Stop offset="1" stopColor="#000000" stopOpacity="0.2" />
+        </LinearGradient>
+      </Defs>
+
+      <Circle fill="#39403A" cx={length / 2} cy={length / 2} r={length / 2 - 2} />
+      <Circle fill="url(#grad)" cx={length / 2} cy={length / 2} r={length / 2 - 2} />
+      <Text
+        x={length / 2}
+        y={length / 2}
+        fontSize={fontSize}
+        textAnchor="middle"
+        fontFamily={vars.fontFamilyMonospace}
+        fill="#9AA39B"
+        dy={fontSize / 3}
+      >
+        {avatarTitle}
+      </Text>
+    </Svg>
   );
 };
-
-const containerStyles = StyleSheet.create({
-  main: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-  },
-  background: {
-    position: 'absolute',
-    backgroundColor: '#39403A',
-    overflow: 'hidden',
-  },
-  gradient: {
-    width: '100%',
-    height: '100%',
-    opacity: 0.2,
-  },
-  xs: {
-    width: 38,
-    height: 38,
-    borderRadius: 19,
-  },
-
-  sm: {
-    width: 42,
-    height: 42,
-    borderRadius: 21,
-  },
-
-  md: {
-    width: 65,
-    height: 65,
-    borderRadius: 32.5,
-  },
-
-  lg_s: {
-    width: 68,
-    height: 68,
-    borderRadius: 33,
-  },
-
-  lg: {
-    width: 80,
-    height: 80,
-    borderRadius: 50,
-  },
-
-  xl: {
-    width: 122,
-    height: 122,
-    borderRadius: 75,
-  },
-});
-
-const textStyles = StyleSheet.create({
-  main: {
-    fontFamily: vars.fontFamilyMonospace,
-    zIndex: 1,
-    color: '#9AA39B',
-    textShadowColor: 'rgba(0, 0, 0, 0.5)',
-    textShadowOffset: { width: 0, height: 0 },
-    textShadowRadius: 3,
-  },
-  xs: {
-    fontSize: 20,
-  },
-  sm: {
-    fontSize: 22,
-  },
-  md: {
-    fontSize: 32,
-  },
-  lg_s: {
-    fontSize: 36,
-  },
-  lg: {
-    fontSize: 42,
-  },
-  xl: {
-    fontSize: 60,
-  },
-});
 
 export default ProfilePicture;
