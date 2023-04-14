7a16ad5fac9a4536123101900ad243d1c6ee7d82
Adrian Gri <33498069+AdrianGri@users.noreply.github.com>
2023-04-13 23:25:36 -0400

    HYP-203: Group messages by date (#111)

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,3 +1,45 @@
+const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+function dateOrdinal(date: number) {
+  if (date > 3 && date < 21) {
+    return 'th';
+  }
+  switch (date % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
+}
+
+export function dateFromTimestamp(timestamp: number) {
+  const date = new Date(timestamp);
+  let dateString = `${monthNames[date.getMonth()]} ${date.getDate()}${dateOrdinal(date.getDate())}`;
+
+  if (date.getFullYear() !== new Date().getFullYear()) {
+    dateString += `, ${date.getFullYear()}`;
+  }
+
+  return dateString;
+}
+
 export function timeSinceTimestamp(timestamp: number) {
   var seconds = Math.floor((Date.now() - timestamp) / 1000);
 
