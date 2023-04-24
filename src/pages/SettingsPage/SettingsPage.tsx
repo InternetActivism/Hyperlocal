796757b18d75a71e7cbbea4df0db413b5ec5e911
Adrian Gri <33498069+AdrianGri@users.noreply.github.com>
2023-04-23 21:35:11 -0400

    HYP-132: New settings page (#117)
    Co-authored-by: Anant <anantsnh@umich.edu>

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,41 +1,100 @@
 import Clipboard from '@react-native-clipboard/clipboard';
-import { Input } from '@rneui/base';
-import { Text } from '@rneui/themed';
+import { Input as BaseInput } from '@rneui/base';
+import { Input, Text } from '@rneui/themed';
 import { useAtom } from 'jotai';
-import React from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import StackHeader from '../../components/common/StackHeader';
-import Button from '../../components/ui/Button';
+import React, { createRef, RefObject, useEffect } from 'react';
+import {
+  Image,
+  ImageSourcePropType,
+  Linking,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { EdgeInsets, useSafeAreaInsets } from 'react-native-safe-area-context';
+import ChevronRightIcon from '../../components/ui/Icons/ChevronRightIcon';
+import CopyIcon from '../../components/ui/Icons/SettingsIcons/CopyIcon';
 import ProfilePicture from '../../components/ui/ProfilePicture';
 import { currentUserInfoAtom } from '../../services/atoms';
 import { CurrentUserInfo } from '../../services/database';
-import { theme } from '../../utils/theme';
+import { theme, vars } from '../../utils/theme';
+
+const ReportIcon = require('../../components/ui/Icons/SettingsIcons/report.png');
+const LogoutIcon = require('../../components/ui/Icons/SettingsIcons/logout.png');
+const HelpIcon = require('../../components/ui/Icons/SettingsIcons/help.png');
+const AboutIcon = require('../../components/ui/Icons/SettingsIcons/about.png');
+const SettingsIcon = require('../../components/ui/Icons/SettingsIcons/settings.png');
+
+type ListItemProps = {
+  imageSource: ImageSourcePropType;
+  title: string;
+  rightView?: JSX.Element;
+  onPress?: () => void;
+};
+
+const ListItem = ({ imageSource, title, rightView, onPress }: ListItemProps): JSX.Element => {
+  const insets = useSafeAreaInsets();
+  const styles = getStyles(insets);
+
+  return (
+    <TouchableOpacity style={styles.listItemContainer} onPress={onPress}>
+      <View style={styles.listItemLeftContainer}>
+        <Image source={imageSource} style={styles.listItemIconContainer} />
+        <Text style={styles.listItemTitle}>{title}</Text>
+      </View>
+      {rightView}
+    </TouchableOpacity>
+  );
+};
 
 const ProfilePage = () => {
   const [currentUserInfo, setCurrentUserInfo] = useAtom(currentUserInfoAtom);
   const [isEditing, setIsEditing] = React.useState(false);
   const [newName, setNewName] = React.useState(currentUserInfo?.nickname);
 
-  const inputContainerStyle = {
-    borderBottomWidth: isEditing ? 1 : 0,
-  };
+  const insets = useSafeAreaInsets();
+  const styles = getStyles(insets);
+
+  const input: RefObject<TextInput & BaseInput> = createRef<TextInput & BaseInput>();
 
   const copyIDToClipboard = () => {
     Clipboard.setString(currentUserInfo?.userID || '');
   };
 
+  const saveName = () => {
+    setIsEditing(false);
+
+    const newUserInfo: CurrentUserInfo = {
+      ...currentUserInfo,
+      nickname: newName,
+      dateUpdated: Date.now(),
+    };
+
+    setCurrentUserInfo(newUserInfo);
+  };
+
+  useEffect(() => {
+    if (isEditing) {
+      input.current?.focus();
+    }
+  }, [isEditing, input]);
+
   return (
-    <SafeAreaView>
-      <StackHeader title="Profile" />
-      <View style={styles.profileContainer}>
+    <View style={styles.pageContainer}>
+      <View style={styles.pill} />
+      <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewContent}>
         {currentUserInfo?.nickname && (
-          <ProfilePicture
-            size="xl"
-            title={currentUserInfo?.nickname || currentUserInfo?.userID || ''}
-          />
+          <View style={styles.ring}>
+            <ProfilePicture
+              size="xl"
+              title={currentUserInfo?.nickname || currentUserInfo?.userID || ''}
+            />
+          </View>
         )}
         <Input
+          ref={input}
           value={newName}
           style={[theme.textTitle, styles.nameText]}
           editable={isEditing}
@@ -43,69 +102,142 @@ const ProfilePage = () => {
             setNewName(text);
           }}
           textAlign="center"
-          inputContainerStyle={inputContainerStyle}
+          inputContainerStyle={styles.inputContainer}
+          onEndEditing={saveName}
         />
-        <TouchableOpacity style={styles.copyContainer} onPress={copyIDToClipboard}>
-          <Text style={styles.uuidText}>{'UUID: ' + currentUserInfo?.userID}</Text>
-        </TouchableOpacity>
-        <View style={styles.buttonContainer}>
-          <Button
-            title={isEditing ? 'Save' : 'Edit'}
-            onPress={() => {
-              if (isEditing && currentUserInfo && newName) {
-                const newUserInfo: CurrentUserInfo = {
-                  ...currentUserInfo,
-                  nickname: newName,
-                  dateUpdated: Date.now(),
-                };
-                setCurrentUserInfo(newUserInfo);
-                setIsEditing(false);
-              } else {
-                setIsEditing(true);
+        <View style={styles.uuidContainer}>
+          <Text style={styles.uuidText} numberOfLines={1}>
+            {'ID: ' + currentUserInfo?.userID}
+          </Text>
+          <TouchableOpacity onPress={copyIDToClipboard}>
+            <CopyIcon />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.listContainer}>
+          <View style={styles.listGroup}>
+            <ListItem
+              imageSource={SettingsIcon}
+              title="Edit Profile"
+              rightView={<View style={styles.forwardButton}>{<ChevronRightIcon />}</View>}
+              onPress={() => setIsEditing(true)}
+            />
+          </View>
+          <View style={styles.listGroup}>
+            <ListItem
+              imageSource={AboutIcon}
+              title="About Us"
+              rightView={<View style={styles.forwardButton}>{<ChevronRightIcon />}</View>}
+              onPress={async () => await Linking.openURL('https://InternetActivism.org')}
+            />
+            <ListItem
+              imageSource={HelpIcon}
+              title="Help & Support"
+              onPress={async () => await Linking.openURL('https://discord.com/invite/29W2yh2d9s')}
+            />
+            <ListItem
+              imageSource={ReportIcon}
+              title="Report a Bug"
+              onPress={async () =>
+                await Linking.openURL(
+                  'https://docs.google.com/forms/d/e/1FAIpQLScWFmUh--oPja-0Nq-wRqaxv5eNejKaSFkntt5Nl7wvl3l39g/viewform?usp=sf_link'
+                )
               }
-            }}
-          />
+            />
+          </View>
+        </View>
+      </ScrollView>
+      <View style={styles.listContainer}>
+        <View style={[styles.listGroup, styles.bottomGroup]}>
+          <ListItem imageSource={LogoutIcon} title="Log Out & Destroy All Data" />
         </View>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  uuidText: {
-    fontSize: 20.8,
-    fontFamily: 'Helvetica',
-    fontWeight: '700',
-    color: '#8A8A8A',
-  },
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 200,
-    alignSelf: 'center',
-  },
-  nameText: {
-    marginTop: 15,
-  },
-  copyContainer: {
-    maxWidth: '80%',
-    borderWidth: 1,
-    padding: 15,
-    marginTop: 10,
-    borderRadius: 10,
-    borderColor: '#8A8A8A',
-  },
-  profileContainer: {
-    alignItems: 'center',
-    marginTop: 26,
-    position: 'relative',
-    height: '100%',
-    width: '100%',
-  },
-  subHeader: {
-    fontSize: 23,
-    fontFamily: 'Rubik-Medium',
-    fontWeight: '500',
-  },
-});
+const getStyles = (insets: EdgeInsets) =>
+  StyleSheet.create({
+    pageContainer: {
+      backgroundColor: vars.backgroundColor,
+      marginBottom: insets.top + insets.bottom,
+      marginTop: 20,
+      alignItems: 'center',
+    },
+    pill: {
+      width: 35,
+      height: 5,
+      backgroundColor: vars.gray.dark,
+    },
+    uuidContainer: {
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: -20,
+    },
+    uuidText: {
+      fontSize: 20.8,
+      fontFamily: 'Helvetica',
+      fontWeight: '700',
+      color: '#8A8A8A',
+      width: '50%',
+    },
+    nameText: {
+      marginTop: 10,
+    },
+    scrollView: {
+      marginTop: 26,
+      position: 'relative',
+      width: '100%',
+      height: '100%',
+    },
+    scrollViewContent: { alignItems: 'center' },
+    ring: {
+      borderWidth: 2,
+      borderColor: '#424242',
+      padding: 3,
+      borderRadius: 70,
+    },
+    listContainer: {
+      width: '100%',
+      paddingHorizontal: 16,
+      flex: 1,
+    },
+    listGroup: {
+      backgroundColor: '#191A19',
+      marginTop: 30,
+      borderRadius: 10,
+    },
+    bottomGroup: { position: 'absolute', bottom: 15, width: '100%', marginHorizontal: 16 },
+    listItemIconContainer: {
+      backgroundColor: '#454D45',
+      height: 30,
+      width: 30,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 7.75,
+      marginRight: 20,
+    },
+    listItemContainer: {
+      width: '100%',
+      paddingHorizontal: 17,
+      paddingVertical: 10,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    listItemLeftContainer: { flexDirection: 'row', alignItems: 'center' },
+    listItemTitle: {
+      fontFamily: vars.fontFamilySecondary,
+      fontWeight: vars.fontWeightMedium,
+      fontSize: 18,
+      color: '#C5C9C5',
+    },
+    forwardButton: {
+      borderRadius: 7.75,
+      marginLeft: 20,
+    },
+    inputContainer: { borderBottomWidth: 0 },
+  });
 
 export default ProfilePage;
