d24cfea8e85bf53fe7b52937f0ce1464b0604eb3
Krish Shah <krish.life21@gmail.com>
2023-05-08 13:26:14 -0700

    Add user plus icon

---
+++ src/components/ui/Icons/AddUserIcon.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+const AddUserIcon = (props: any) => (
+  <Svg
+    width={43}
+    height={42}
+    viewBox="0 0 43 42"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <Path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M17.6971 26.6108C10.8099 26.6108 4.92786 27.6276 4.92786 31.7016C4.92786 35.7738 10.7723 36.8291 17.6971 36.8291C24.5842 36.8291 30.4663 35.8106 30.4663 31.7383C30.4663 27.6661 24.6219 26.6108 17.6971 26.6108Z"
+      stroke="#707770"
+      strokeWidth={3}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M17.697 20.8003C22.2407 20.8003 25.8814 17.2426 25.8814 12.8063C25.8814 8.36832 22.2407 4.81232 17.697 4.81232C13.1552 4.81232 9.5127 8.36832 9.5127 12.8063C9.5127 17.2426 13.1552 20.8003 17.697 20.8003Z"
+      stroke="#707770"
+      strokeWidth={3}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M34.4074 15.1709V22.1884"
+      stroke="#707770"
+      strokeWidth={3}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M38.073 18.6795H30.7451"
+      stroke="#707770"
+      strokeWidth={3}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Svg>
+);
+export default AddUserIcon;
